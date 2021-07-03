c821a040cfef3dd87c146c92c1c8beb519785600
srisrinu1 <srinivasreddychalla133@gmail.com>
2021-07-03 08:43:12 +0530

    commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const inputBox = document.querySelector(".inputfield input");
+const addBtn = document.querySelector(".inputfield button");
+inputBox.onkeyup = () => {
+    let UserData = inputBox.Value;
+    if (UserData.trim()!=0){
+        
+    }
+}
